0219c9bae5b1975ce7889c975ce3579fb907f8e4
Fonguh Blaise Tanam <watigamer99@gmail.com>
2022-05-11 02:15:14 +0000

    commit

---
+++ projects/Sololearn Exercices/Dictionary Functions.py
@@ -0,0 +1,13 @@
+books = {
+    "Life of Pi": "Adventure Fiction",
+    "The Three Musketeers": "Historical Adventure",
+    "Watchmen": "Comics",
+    "Bird Box": "Horror",
+    "Harry Potter": "Fantasy Fiction",
+    "Good Omens": "Comedy"
+}
+
+book = input()
+
+# change this part to use the .get() method
+print(books.get(book, "Book not found"))
