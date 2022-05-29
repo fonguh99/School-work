38c97b8024484d803a587468d1bb87fcfbceeda8
Fonguh Blaise Tanam <watigamer99@gmail.com>
2022-05-29 23:38:42 +0000

    commit

---
+++ projects/CSM 158- C++/Palindrome Numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+bool isPalindrome(int x)
+{
+    // complete the function
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    if (isPalindrome(n))
+    {
+        cout << n << " is a palindrome";
+    }
+    else
+    {
+        cout << n << " is NOT a palindrome";
+    }
+    return 0;
+}
