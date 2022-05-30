afceda022706b88bb5eb02132c6b1dda8140cf4f
Fonguh Blaise Tanam <watigamer99@gmail.com>
2022-05-30 23:49:41 +0000

    commit

---
+++ projects/CSM 158- C++/Battle of the cakes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int recSum(int n)
+{
+    // complete the function
+}
+
+int main()
+{
+    // getting input
+    int n;
+    cin >> n;
+
+    // call the recursive function and print returned value
+
+    return 0;
+}
---
+++ projects/CSM 158- C++/Calculating sums.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// complete the function
+void add()
+{
+}
+// overload it to sum doubles
+void add()
+{
+}
+
+int main()
+{
+
+    // calling
+    add(5, 6);
+    add(1.2, 6.5);
+
+    return 0;
+}
