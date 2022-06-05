be3d7bb22fd28987c20b620b334d91f30e7fa7b0
Fonguh Blaise Tanam <watigamer99@gmail.com>
2022-06-05 23:36:38 +0000

    commit

---
+++ projects/CSM 158- C++/Square matrix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    string arr[3][3] = {
+        {"Python", "JS", "C++"},
+        {"PHP", "SQL", "Java"},
+        {"C#", "Swift", "Kotlin"},
+    };
+    // your code goes here
+
+    return 0;
+}
