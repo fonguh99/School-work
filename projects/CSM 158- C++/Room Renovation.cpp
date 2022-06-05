72f8364204f8de36725c71e58feb6be84cee129d
Fonguh Blaise Tanam <watigamer99@gmail.com>
2022-06-05 23:31:39 +0000

    commit

---
+++ projects/CSM 158- C++/Room Renovation.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // sides of the room
+    double length;
+    double width;
+    cin >> length;
+    cin >> width;
+
+    // output the area
+
+    return 0;
+}
