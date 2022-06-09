184173cd773964eeff83ffebf3d99dc987c03dab
Fonguh Blaise Tanam <watigamer99@gmail.com>
2022-06-09 22:41:41 +0000

    commit

---
+++ projects/CSM 158- C++/Chirp Chirp.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+class Bird
+{
+    // complete the class, add makeSound() method
+};
+
+int main()
+{
+
+    // instantiation
+    Bird bird;
+
+    // function call
+    bird.makeSound();
+
+    return 0;
+}
